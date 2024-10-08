2038d81b7485b1ce7b96943b14aa8462ece5f540
eniola <eniolafaithful57@gmail.com>
2024-10-08 20:50:43 +0100

    feat: add function to nable editing and getting tasks

--- src/action/task.ts
+++ src/action/task.ts
@@ -1,9 +1,14 @@
 "use server";
 
 import { db } from "@/server/db";
-import { createTask, createTaskGroup } from "@/server/db/queries/insert";
+import {
+  createTask,
+  createTaskGroup,
+  updateTask,
+} from "@/server/db/queries/insert";
 import { type InsertTaskGroup } from "@/server/db/schema";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function addTaskGroup(data: InsertTaskGroup) {
   await createTaskGroup(data);
@@ -20,7 +25,6 @@ export async function addTask(
   const reasonForResource = formData.get("reasonForResource") as string;
   try {
     await createTask({ taskGroupId: id, name, resource, reasonForResource });
-    revalidatePath("/");
 
     return { message: "Task added successfully" };
   } catch (error) {
@@ -28,7 +32,23 @@ export async function addTask(
   }
 }
 
-export async function getTasksFromGroup(id: number) {
+export async function editTask(
+  id: number,
+  prevState: unknown,
+  formData: FormData,
+) {
+  const name = formData.get("name") as string;
+  const resource = formData.get("resource") as string;
+  const reasonForResource = formData.get("reasonForResource") as string;
+  try {
+    await updateTask({ id, name, resource, reasonForResource });
+    return { message: "Task updated successfully" };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const getTasksFromGroup = async (id: number) => {
   const tasks = await db.query.taskGroups.findFirst({
     where: (taskGroups, { eq }) => eq(taskGroups.id, id),
     with: {
@@ -36,4 +56,11 @@ export async function getTasksFromGroup(id: number) {
     },
   });
   return tasks;
+};
+
+export async function getTask(id: number) {
+  const task = await db.query.tasks.findFirst({
+    where: (tasks, { eq }) => eq(tasks.id, id),
+  });
+  return task;
 }

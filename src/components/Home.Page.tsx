e484aa132b085a806be19bd38540d445daa235b4
eniola <eniolafaithful57@gmail.com>
2024-10-08 20:56:57 +0100

    feat: implement functionality to the home page

--- src/components/Home.Page.tsx
+++ src/components/Home.Page.tsx
@@ -1,45 +1,32 @@
-"use client";
-
-import { useState } from "react";
 import Goals from "./Goals";
-import DialogWrapper from "./DialogWrapper";
+import GroupDialogWrapper from "./GroupDialogWrapper";
+import { type SelectTaskGroup } from "@/server/db/schema";
 
-export default function Home() {
-  const [display, setDisplay] = useState("empty");
+interface HomeProps {
+  postGroups: SelectTaskGroup[];
+}
+
+export default function Home({ postGroups }: HomeProps) {
   return (
     <>
-      {/* <Progress /> */}
-      {display === "empty" && (
+      {postGroups.length === 0 && (
         <>
           <div className="flex h-[calc(100dvh-80px)] w-full min-w-[2rem] items-center justify-center text-2xl text-[#444444]">
             There are not any tasks available yet, creat one below with the icon
             :)
           </div>
           <div className="flex justify-center">
-            <DialogWrapper>
+            <GroupDialogWrapper >
               <button className="absolute bottom-[4rem] mx-auto flex h-[40px] w-[40px] items-center justify-center rounded-full bg-blue-700 p-1 px-2">
                 <PlusIcon />
               </button>
-            </DialogWrapper>
+            </GroupDialogWrapper>
           </div>
         </>
       )}
 
-      {display === "select" && <Goals />}
+      {postGroups.length > 0 && <Goals postGroups={postGroups} />}
     </>
-  );
-}
-
-function Progress() {
-  return (
-    <div className="flex items-center py-5">
-      {/* <span className="w-fit mr-4">Today</span> */}
-      <div className="">
-        <ProgressBar />
-      </div>
-      <span>You have completed 3 of the 8 tasks planned today</span>
-      {/* <span className="w-[4rem] flex justify-end">50%</span> */}
-    </div>
   );
 }
 
@@ -54,17 +41,5 @@ function PlusIcon() {
     >
       <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>
     </svg>
-  );
-}
-
-function ProgressBar() {
-  return (
-    <div className="progress-bar h-[2rem] min-w-[300px] rounded-md bg-blue-200">
-      <div
-        className="progress h-full rounded-bl-md rounded-tl-md bg-blue-600"
-        style={{ width: "20%" }}
-      ></div>
-      20%
-    </div>
   );
 }
--- src/components/TasKGroupCard.tsx
+++ src/components/TasKGroupCard.tsx
@@ -1,9 +1,10 @@
+import { type SelectTaskGroup } from "@/server/db/schema";
 import { Folder } from "lucide-react";
 import React from "react";
 
 const colorMix = [
   {
-    light: "##d0f9e7",
+    light: "#c3f7e1",
     dark: "#49966b",
   },
   {
@@ -11,21 +12,33 @@ const colorMix = [
     dark: "#dd7836",
   },
   {
-    light: "#e0f2fe",
+    light: "#acd3ec",
     dark: "#2064e4",
   },
 ];
 
-const TasKGroupCard = ({dark, light}: {dark: string, light: string}) => {
+type Props = { title: string, currentGroup: SelectTaskGroup, setCurrentTaskGroup: React.Dispatch<React.SetStateAction<SelectTaskGroup>> };
+
+export const TasKGroupCard = ({ title, currentGroup, setCurrentTaskGroup }: Props) => {
+  const randomColor = colorMix[Math.floor(Math.random() * colorMix.length)]!;
+  const style = {
+    "--light-color": randomColor.light,
+    "--dark-color": randomColor.dark,
+  } as React.CSSProperties;
   return (
-    <div className="p-4 space-y-3 w-1/3 rounded-md">
-      <div className="flex gap-3 items-center">
-        <Folder />
-        <span> Interactions for Watch</span>    
+    <div
+      style={style}
+      className="w-1/3 flex flex-col justify-between space-y-3 rounded-md bg-[var(--light-color)] p-4"
+    >
+      <div className="flex items-center gap-3">
+        <Folder style={style} className="fill-[var(--dark-color)] stroke-transparent" />
+        <span> {title}</span>
       </div>
-      <div className="flex justify-between items-center">
-        <span className="">3 tasks</span>
-        <span>Details</span>
+      <div className="flex items-center justify-between">
+        <span style={style} className="text-[var(--dark-color)]">
+          3 tasks
+        </span>
+        <button onClick={() => setCurrentTaskGroup(currentGroup)} className="text-sm underline text-[#444444] text-opacity-65">Details</button>
       </div>
     </div>
   );

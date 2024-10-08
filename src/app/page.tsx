2afb7f66e6be0904972ca84162375c0568715c98
eniola <eniolafaithful57@gmail.com>
2024-10-08 20:51:25 +0100

    feat: enable that we only collocate tasks created in on the current day is displayed on the home dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,25 @@ import Home from "@/components/Home.Page";
 import { db } from "@/server/db";
 
 export default async function HomePage() {
-  // const posts = await db.query.posts.findMany();
-  // console.log(posts);
+
+
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+
+  // Get the end of the day
+  const endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999);
+
+  console.log(startOfDay, endOfDay);
+  const postGroups = await db.query.taskGroups.findMany({
+    where: (taskGroups, { lte, gte }) =>
+      lte(taskGroups.createdAt, endOfDay) &&
+      gte(taskGroups.createdAt, startOfDay),
+  });
+  console.log(postGroups);
   return (
     <main className="">
-      <Home />
+      <Home postGroups={postGroups} />
     </main>
   );
 }

2a5bcf442d0b6ffb8dd97d84252518b02c02151e
eniola <eniolafaithful57@gmail.com>
2024-10-12 23:44:45 +0100

    feat: refactored stucture of my code to accomadate room for a landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,7 @@
-import Home from "@/components/Home.Page";
-import { db } from "@/server/db";
+import React from "react";
 
-export default async function HomePage() {
+const Homepage = () => {
+  return <div>This is for the landing page</div>;
+};
 
-
-  const startOfDay = new Date();
-  startOfDay.setHours(0, 0, 0, 0);
-
-  // Get the end of the day
-  const endOfDay = new Date();
-  endOfDay.setHours(23, 59, 59, 999);
-
-  console.log(startOfDay, endOfDay);
-  const postGroups = await db.query.taskGroups.findMany({
-    where: (taskGroups, { lte, gte }) =>
-      lte(taskGroups.createdAt, endOfDay) &&
-      gte(taskGroups.createdAt, startOfDay),
-  });
-  console.log(postGroups);
-  return (
-    <main className="">
-      <Home postGroups={postGroups} />
-    </main>
-  );
-}
+export default Homepage;

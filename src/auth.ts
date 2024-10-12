52ddba7023d77c8b2df31249869a9056a97b8594
eniola <eniolafaithful57@gmail.com>
2024-10-12 23:14:54 +0100

    feat: implemet google authentication with next-auth

---
+++ src/auth.ts
@@ -0,0 +1,37 @@
+import Google from "next-auth/providers/google";
+import NextAuth, { type DefaultSession } from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./server/db";
+import { accounts, sessions, users } from "./server/db/schema";
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      id: string;
+      name: string;
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession["user"];
+  }
+}
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+  }),
+  providers: [Google],
+  callbacks: {
+    async session({ session, user, token }) {
+      return session;
+    },
+  },
+});

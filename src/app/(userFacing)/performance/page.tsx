a7aae35ebce0fc735a49a8b4bb3ccbbad9a04d97
eniola <eniolafaithful57@gmail.com>
2024-10-14 18:39:49 +0100

    feat: fetch necessary chart data in the performance page

--- src/app/(userFacing)/performance/page.tsx
+++ src/app/(userFacing)/performance/page.tsx
@@ -7,26 +7,56 @@ import RadialCompareWrapper from "./components/RadialCompare";
 import MultipleLineChart from "./components/MultipleLineChart";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import SelectScrollable from "./components/SelectWeek";
+import MultipleRadialChart from "./components/MultipleRadialChart";
+import { formatAreaChartData, getWeekNumber } from "@/lib/helpers";
+import {
+  getBestPerformingWeek,
+  getCompletedTasksInWeek,
+  getWeekGroupTasks,
+} from "@/server/db/queries/select";
 
 const PerformancePage = async () => {
   const session = await auth();
 
   if (!session?.user) redirect("/login");
+  const today = new Date();
+  const currentWeekString = `${getWeekNumber(today)}-${today.getFullYear()}`;
+  const lastWeekDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - 7,
+  );
+  const lastWeekString = `${getWeekNumber(lastWeekDate)}-${lastWeekDate.getFullYear()}`;
+  const lastWeek = await getCompletedTasksInWeek(lastWeekString);
+  const currentWeek = await getCompletedTasksInWeek(currentWeekString);
+  const chartData = formatAreaChartData(currentWeek, undefined, lastWeek);
+
+  const weekOfBestPerformance = await getBestPerformingWeek();  
+  const bestWeek = await getCompletedTasksInWeek(weekOfBestPerformance);
+
+  const currentWeekVsBestChart = formatAreaChartData(currentWeek, bestWeek);
+  console.log(currentWeekVsBestChart, "currentWeekVsBestChart");
+
+  console.log(chartData, "chartData");
   return (
     <div className="px-3">
-      <h2 className="py-2 text-lg font-semibold">
-        Performance Characterization
-      </h2>
+      <div className="flex items-center justify-between">
+        <h2 className="py-3 text-lg font-semibold">
+          Performance Characterization
+        </h2>
+        <SelectScrollable />
+      </div>
       <ScrollArea className="h-[calc(100dvh-8rem)] pr-4">
         <div className="grid grid-cols-[30%_30%_auto] gap-4">
           {/* comparting this week's completed task with last week */}
           <div className="col-span-full grid grid-cols-2 gap-4">
             <div>
-              <MultipleLineChart />
+              <MultipleLineChart chartData={chartData!} />
             </div>
             {/* comparting this week's completed task with our best week(in terms of completed tasks) */}
             <div>
-              <AreaChartWrapper />
+              <AreaChartWrapper chartData={currentWeekVsBestChart!} />
             </div>
           </div>
           {/* comparing the number of tasks planned each day for the past 3 months */}
@@ -45,6 +75,9 @@ const PerformancePage = async () => {
           {/** you've planned 20 more taks this week than your best ever tally*/}
           <div>
             <RadialCompareWrapper />
+          </div>
+          <div>
+            <MultipleRadialChart />
           </div>
         </div>
       </ScrollArea>

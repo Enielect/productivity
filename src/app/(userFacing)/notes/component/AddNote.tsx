090b8ec8d96d313426a3ccf45ecd768411e68900
eniola <eniolafaithful57@gmail.com>
2024-10-14 11:58:36 +0100

    feat: finally got the layout of the  form to add note to work properly, mostly my just include position: fixed

--- src/app/(userFacing)/notes/component/AddNote.tsx
+++ src/app/(userFacing)/notes/component/AddNote.tsx
@@ -12,11 +12,11 @@ function AddNote() {
   const [state, action] = useFormState(addNote, undefined);
   return (
       <form action={action}
-        className="fixed h-[200px] mx-auto w-[calc(100%-440px)] transition-all data-[open=false]:-bottom-[135px] data-[open=true]:bottom-[60px]"
+        className="fixed h-[200px]  mx-auto w-[calc(100%-440px)] transition-all data-[open=false]:-bottom-[135px] data-[open=true]:bottom-[60px]"
         data-open={String(showAddNote)}
       >
-        <div className="flex justify-center text-center">
-          <div className="bottom-0 w-3/4 max-w-[37.5rem] space-y-4 rounded-md">
+        <div className="flex justify-center  text-center">
+          <div className="bottom-0 w-3/4  max-w-[37.5rem] space-y-4 rounded-md">
             <button
               className="rounded-full bg-blue-600 p-1"
               onClick={() => setShowAddNote((c) => !c)}
@@ -27,7 +27,7 @@ function AddNote() {
                 <ChevronsDown className="stroke-white" />
               )}
             </button>
-            <div className="space-y-4 rounded-md border p-2">
+            <div className="space-y-4 bg-white rounded-md border p-2">
               <div>Input your message here</div>
               <Input name="title" placeholder="title" required />
               <div className="grid w-full gap-2 space-y-2">

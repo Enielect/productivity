db5dafcfc3bf685c9a703b4795a3602a98420afd
eniola <eniolafaithful57@gmail.com>
2024-10-17 04:01:29 +0100

    feat: Linked the calendar to take user to the report for the day selected

--- src/components/RightCalendar.tsx
+++ src/components/RightCalendar.tsx
@@ -1,11 +1,24 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
-import { Calendar } from "@/components/ui/calendar"
+import { Calendar } from "@/components/ui/calendar";
+import { useRouter } from "next/navigation";
+import { getWeekNumber } from "@/lib/helpers";
 
 export function RightCalendar() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date())
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const router = useRouter();
+  const currentWeekString = `${getWeekNumber(date!)}-${date!.getFullYear()}`;
+  const reg = /\//g;
+  const dayFormat = date?.toLocaleDateString().replace(reg, "-");
+
+  //This was unintentional. I remembered that javascript compares obnects by reference,
+  //but again, I wouldn't want to call the router.push function in the render method.
+  //I would rather use a useEffect hook to handle this.(MAYBE THIS WOULD BE BETTER)
+  if (date !== new Date()) {
+    router.push(`/reports/${currentWeekString}/${dayFormat}`);
+  }
 
   return (
     <Calendar
@@ -14,5 +27,5 @@ export function RightCalendar() {
       onSelect={setDate}
       className="rounded-md border"
     />
-  )
+  );
 }

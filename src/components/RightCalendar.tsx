827cff6d31a99974b99ad7d9b5345731482af0a5
eniola <eniolafaithful57@gmail.com>
2024-10-19 10:56:38 +0100

    feat: update right calendar compenent to act properly by preventing users from viewing the report of the actual day since I am using a useEffect, which runs on mount and would mean that when a user first vists the app, the route segment specified in the router.push() function is automatically visited

--- src/components/RightCalendar.tsx
+++ src/components/RightCalendar.tsx
@@ -9,6 +9,7 @@ import { getWeekNumber } from "@/lib/helpers";
 export function RightCalendar() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
   const router = useRouter();
+  console.log(date, "date");
   const currentWeekString = `${getWeekNumber(date!)}-${date!.getFullYear()}`;
   const reg = /\//g;
   const dayFormat = date?.toLocaleDateString().replace(reg, "-");
@@ -16,9 +17,11 @@ export function RightCalendar() {
   //This was unintentional. I remembered that javascript compares obnects by reference,
   //but again, I wouldn't want to call the router.push function in the render method.
   //I would rather use a useEffect hook to handle this.(MAYBE THIS WOULD BE BETTER)
-  if (date?.toLocaleDateString() !== new Date().toLocaleDateString()) {
-    router.push(`/reports/${currentWeekString}/${dayFormat}`);
-  }
+  React.useEffect(() => {
+    if (date?.toLocaleDateString() !== new Date().toLocaleDateString()) {
+      router.push(`/reports/${currentWeekString}/${dayFormat}`);
+    }
+  }, [date, router, currentWeekString, dayFormat]);
 
   return (
     <Calendar

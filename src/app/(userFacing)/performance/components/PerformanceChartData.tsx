07ae94dbbceda71334175a60740a432e03e4faa9
eniola <eniolafaithful57@gmail.com>
2024-10-19 10:53:39 +0100

    feat: create compnents that make the performance chart data component reusable

---
+++ src/app/(userFacing)/performance/components/PerformanceChartData.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+import AreaChartWrapper from "./AreaChart";
+import RadialchartWrapper from "./RadialChart";
+import LongLineChart from "./LongLineChart";
+import RadialCompareWrapper from "./RadialCompare";
+import MultipleRadialChart from "./MultipleRadialChart";
+
+import MultipleLineChart from "./MultipleLineChart";
+import RadialCompareChart from "./RadialCompareChart";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+const PerformanceChartData = ({
+  chartData,
+  currentWeekVsBestChart,
+  tasksPerDayChart,
+  thisWeekVsLastWeekChart,
+  thisWeekEfficiency,
+  thisWeekVsBestWeekChart,
+}: {
+  chartData:
+    | {
+        day: string;
+        currentWeek: number;
+        bestWeek: number;
+      }[]
+    | {
+        day: string;
+        currentWeek: number;
+        lastWeek: number;
+      }[]
+    | undefined;
+
+  currentWeekVsBestChart:
+    | {
+        day: string;
+        currentWeek: number;
+        bestWeek: number;
+      }[]
+    | {
+        day: string;
+        currentWeek: number;
+        lastWeek: number;
+      }[]
+    | undefined;
+  tasksPerDayChart: {
+    date: string;
+    completedTasks: number | undefined;
+  }[];
+  thisWeekVsLastWeekChart: {
+    thisWeek: number;
+    lastWeek: number;
+  }[];
+  thisWeekVsBestWeekChart: {
+    week: string;
+    completed: number;
+    fill: string;
+  }[];
+  thisWeekEfficiency: number;
+}) => {
+  return (
+    <ScrollArea className="h-[calc(100dvh-5rem)] w-screen pb-[60px] pr-6 pt-2 md:w-full md:pb-9">
+      <div className="space-y-3 md:grid md:grid-cols-[30%_30%_auto] md:gap-4 md:space-y-0">
+        {/* comparting this week's completed task with last week */}
+        <div className="grid gap-4 md:col-span-full md:grid-cols-2">
+          {/* percentage of tasks completed per day */}
+          <div>
+            <MultipleLineChart chartData={chartData!} />
+          </div>
+          {/* comparting this week's completed task with our best week(in terms of completed tasks) */}
+          <div>
+            <AreaChartWrapper chartData={currentWeekVsBestChart!} />
+          </div>
+        </div>
+        {/* comparing the number of tasks planned each day for the past 3 months */}
+        <div className="col-span-full">
+          <LongLineChart chartData={tasksPerDayChart} />
+        </div>
+        {/**you've planned 20 more tasks this weeek than last week */}
+        <div>
+          <RadialCompareChart chartData={thisWeekVsLastWeekChart} />
+        </div>
+
+        {/**progress with completing set tasks for the current week(efficiency) */}
+        <div>
+          <RadialchartWrapper
+            chartData={[
+              { label: "Efficiency", efficiency: thisWeekEfficiency },
+            ]}
+          />
+        </div>
+        {/** you've planned 20 more taks this week than your best ever tally*/}
+        {/* <div>
+            <RadialCompareWrapper />
+          </div> */}
+        <div>
+          <MultipleRadialChart chartData={thisWeekVsBestWeekChart} />
+        </div>
+      </div>
+    </ScrollArea>
+  );
+};
+
+export default PerformanceChartData;

c3d5bea821d2ac7dbabf9627956f7754ab9eabd7
eniola <eniolafaithful57@gmail.com>
2024-10-19 06:22:56 +0100

    feat: try to be strict with user operation such as delete, update, create, e.t.c to avoid unintended collisions

--- src/server/db/queries/insert.ts
+++ src/server/db/queries/insert.ts
@@ -1,11 +1,6 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "..";
-import {
-  type InsertTask,
-  notes,
-  taskGroups,
-  tasks,
-} from "../schema";
+import { type InsertTask, notes, taskGroups, tasks } from "../schema";
 import { auth } from "@/auth";
 
 type EditProps = {
@@ -13,44 +8,73 @@ type EditProps = {
   content: string;
   id: number;
 };
-const session = await auth();
 
-export async function createTaskGroup(data: {name: string}) {
-  if(session?.user) await db.insert(taskGroups).values({...data, userId: session.user.id});
+export async function createTaskGroup(data: { name: string }) {
+  const session = await auth();
+  if (session?.user)
+    await db.insert(taskGroups).values({ ...data, userId: session.user.id });
 }
 
 export async function createTask(data: InsertTask) {
-  if(session?.user) await db.insert(tasks).values(data);
+  const session = await auth();
+  if (session?.user) await db.insert(tasks).values(data);
 }
 
 export async function createSummary(taskId: number, summary: string) {
-  if(session?.user) await db.update(tasks).set({ summary }).where(eq(tasks.id, taskId));
+  const session = await auth();
+  if (session?.user)
+    await db.update(tasks).set({ summary }).where(eq(tasks.id, taskId));
 }
 
 export async function updateTask(data: InsertTask) {
-  if(session?.user) await db.update(tasks).set(data).where(eq(tasks.id, data.id!));
+  const session = await auth();
+  if (session?.user)
+    await db.update(tasks).set(data).where(eq(tasks.id, data.id!));
 }
 
 export async function editCheckedTask(isChecked: boolean, taskId: number) {
-  if(session?.user) await db.update(tasks).set({ isChecked }).where(eq(tasks.id, taskId));
+  const session = await auth();
+  if (session?.user)
+    await db.update(tasks).set({ isChecked }).where(eq(tasks.id, taskId));
 }
 
-export async function deleteTask(taskId: number) {
-  if(session?.user) await db.delete(tasks).where(eq(tasks.id, taskId));
+export async function deleteTask(taskId: number, taskGroupId: number) {
+  const session = await auth();
+  if (session?.user)
+    await db.delete(tasks).where(
+      and(
+        eq(tasks.id, taskId),
+        eq(tasks.taskGroupId, taskGroupId), // eq(tasks.userId, session.user.id)
+      ),
+    );
 }
 
 export async function deleteTaskGroup(taskGroupId: number) {
-  if(session?.user) await db.delete(taskGroups).where(eq(taskGroups.id, taskGroupId));
+  const session = await auth();
+  if (session?.user)
+    await db
+      .delete(taskGroups)
+      .where(
+        and(
+          eq(taskGroups.id, taskGroupId),
+          eq(taskGroups.userId, session.user.id),
+        ),
+      );
 }
 
-export async function createNote(data: {title: string, content: string}) {
-  if(session?.user) await db.insert(notes).values({userId: session.user.id, ...data});
+export async function createNote(data: { title: string; content: string }) {
+  const session = await auth();
+  if (session?.user)
+    await db.insert(notes).values({ userId: session.user.id, ...data });
 }
 
 export async function editNote(data: EditProps) {
-  if(session?.user) await db.update(notes).set(data).where(eq(notes.id, data.id));
+  const session = await auth();
+  if (session?.user)
+    await db.update(notes).set(data).where(eq(notes.id, data.id));
 }
 
 export async function deleteNote(id: number) {
-  if(session?.user) await db.delete(notes).where(eq(notes.id, id));
+  const session = await auth();
+  if (session?.user) await db.delete(notes).where(eq(notes.id, id));
 }

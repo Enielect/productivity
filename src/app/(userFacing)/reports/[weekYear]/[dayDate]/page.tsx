67e376624e1be8310a31d626c5c3ccb3f929d898
eniola <eniolafaithful57@gmail.com>
2024-10-19 06:21:09 +0100

    feat: cleanup and rename function name

--- src/app/(userFacing)/reports/[weekYear]/[dayDate]/page.tsx
+++ src/app/(userFacing)/reports/[weekYear]/[dayDate]/page.tsx
@@ -15,22 +15,18 @@ const DayPage = async ({ params }: Params) => {
   const formatParams = params.dayDate.split("-").join("/");
   const dayTasks = await getDayGroupTasks(weekTasks!, formatParams);
   const {
-    gridGradientChart,
+    totalTasksCreatedPerTaskGroup,
     pieChart,
     radialChartEfficiency,
     comparePlannedVsExecuted,
     completedLongLineChart,
   } = await completedTasksInDay(formatParams);
-  // console.log(gridGradientChart, "gridGradientChart");
-  // console.log(pieChart, "pieChart");
-  // console.log(radialChartEfficiency, "radialChartEfficiency");
-  // console.log(comparePlannedVsExecuted, "comparePlannedVsExecuted");
-  // console.log(completedLongLineChart, "completedLongLineChart");
+
   return (
     <div>
       <TaskGroupWrapper
         chartData={{
-          gridGradientChart,
+          totalTasksCreatedPerTaskGroup,
           pieChart,
           radialChartEfficiency,
           comparePlannedVsExecuted,
